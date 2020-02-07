fcb5ae6f0c330ee07ea5c063dd166e997f66a8bb
fastandfurios <semu-dima@yandex.ru>
2020-02-07 10:01:35 +0300

    :wrench: настройка приложения под портретный режим, мелкие доработки

--- sanitary.app/sanitary.app.Android/MainActivity.cs
+++ sanitary.app/sanitary.app.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace sanitary.app.Droid
 {
-    [Activity(Label = "sanitary.app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "sanitary.app", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
